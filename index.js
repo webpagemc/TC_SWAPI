8f0901a30d689855aa74d4f7593747a0081531b2
MauroGC2222 <90658076+MauroGC2222@users.noreply.github.com>
2022-11-26 21:38:14 -0300

    add axios dependence
    change character style
    
    add route character/:url

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 const PORT = process.env.PORT || 1002;
@@ -9,6 +10,20 @@ const PORT = process.env.PORT || 1002;
 app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
+
+//react config
+
+app.get("/character/:url",(req,res)=>{
+
+    const url = req.params.url;
+    const id = url.split("/").pop()
+   
+    axios.get(`https://swapi.dev/api/people/${id}`)
+    .then((character)=>{character.json()})
+    .then((data)=>{res.json({data})})
+
+})
+
 
 //static files
 app.use(express.static("./src/public"));
