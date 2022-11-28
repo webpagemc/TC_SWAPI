647e892b61e6d59ca88b086ab77e3526b6fde5d8
MauroGC2222 <90658076+MauroGC2222@users.noreply.github.com>
2022-11-28 15:35:42 -0300

    solve render error in component character.jsx

--- src/react/components/character/character.jsx
+++ src/react/components/character/character.jsx
@@ -7,7 +7,7 @@ export default function Character(props){
     const id = url.split("/")[5]
 
     return(
-        <a href={`http://tc_swapi_maucalodolce20.vercel.app/character/${id}`} className="character_container">
+        <a href={`https://tc-swapi-maucalodolce20.herokuapp.com/character/${id}`} className="character_container">
             
             <div className="character_name">{props.name}</div>
             <div className="character_birthday"><b>Age:</b>{props.birthday}</div>
